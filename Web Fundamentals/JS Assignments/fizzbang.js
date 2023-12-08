386f458fdcc403fd2309f9f423e842c381bfa16e
ejett5 <minijett5@gmail.com>
2023-12-08 16:35:03 -0500

    added folder organisation

---
+++ Web Fundamentals/JS Assignments/fizzbang.js
@@ -0,0 +1,11 @@
+//declare variable
+var array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+//set up if loop to look for every 3rd and 5th multiple 
+if(array % 3 && item.array % 5){
+    console.log(FizzBuzz)
+}else{
+    if( item.array % 3 || item.array % 5){
+        console.log(Fizz, Buzz)
+    }
+}
