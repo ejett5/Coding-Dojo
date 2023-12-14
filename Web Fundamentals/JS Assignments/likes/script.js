fa89753eb84fcdc220b1a9d77cd98dbc0794f59d
ejett5 <minijett5@gmail.com>
2023-12-14 15:42:37 -0500

    updated like function

--- Web Fundamentals/JS Assignments/likes/script.js
+++ Web Fundamentals/JS Assignments/likes/script.js
@@ -3,15 +3,28 @@ console.log("test script")
 
 // setting up counter for like button
 //naming the function
-var button = document.getElementById("clicked"),
-    count = 0;
+var button = document.getElementById("clicked")
+    //count = 0;
 //connecting to the button input
-function clicked() {
+// function clicked() {
     
-    //after each click send to a display
-    count += 1
-    //display each click starting from 0 and tallying click of the button
-    button.innerText = "clicked" + count;
-};
+//     //after each click send to a display
+//     count += 1
+//     //display each click starting from 0 and tallying click of the button
+//     button.innerText = "clicked" + count;
+// };
 
 
+//syntax according to language documentation and does not work
+// element.addEventListener("clicked", likes);
+// function likes(){
+//     button = document.getElementById("clicked");
+//     count ++;
+//     button.innerText = likes + count
+// }
+
+
+function likes(selector){
+   // var element = document.querySelector(selector)
+    selector.innerText++;
+}
