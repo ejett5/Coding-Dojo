450000e17fc47996d142ec5f2d87e3797c3a2962
ejett5 <minijett5@gmail.com>
2024-01-05 14:59:55 -0500

    init upload for py folder for coding dojo

--- Web Fundamentals/CSS Assign/Algo/22December.js
+++ Web Fundamentals/CSS Assign/Algo/22December.js
@@ -20,7 +20,7 @@ const expectedA = [3,4]
 /*
 Explanation: We calculate the values as follows:
 - The elements at indices 1, 2, and 3 in nums1 occur at least once in nums2. So the first value is 3.
-- The elements at indices 0, 1, 3, and 4 in nums2 occur at least once in nums1. So the second value is 4.
+- The elements at indices 0, 1, 3, and 4 in nums2 occur at least once in nums1. So the second value is 4.  *location where the value appears in the second index
 
 
 Example 2:
