d23bb424bdf3552e40c0cd80cb4315276cd7e1a4
ejett5 <minijett5@gmail.com>
2024-01-10 17:16:34 -0500

    more bugs discovered when updating user info

--- Python/fundamentals/oop/bank_account.py
+++ Python/fundamentals/oop/bank_account.py
@@ -41,8 +41,15 @@ class BankAccount:
         
 jm_toyota = BankAccount(18, 100, 20, 1, 3.5)  #made as strings to test and made no difference on call
 jack_black = BankAccount(3, 55, 100, 80, 3)
-pedro_pascal = BankAccount(180, 1, 400, 142, 10)
+pedro_pascal = BankAccount(180, 1, 400, 142, 10) #TODO figure out why this stop functioning from col 39 on
 print(jm_toyota.display_account_info(), jack_black.display_account_info(), pedro_pascal.display_account_info())
+
+#updating info to user accounts
+jm_toyota.deposit() == 50
+# jm_toyota.deposit() == 50
+print(jm_toyota.display_account_info()) #adding this section of code causes the pedro_pascal to have issues printing from the deposit att
+
+
 # print(f"jm_toyota.display_account_info() + jack_black.display_account_info(), pedro_pascal.display_account_info()")  #just playing around to see if f string can be pushed in and combine multiples
 # jm_toyota.withdraw(200).yield_interest_rate(5).display_account_info()  #TODO why is int not callable?
 # jack_black.deposit(55).deposit(50).withdraw(1).withdraw(1).withdraw(1).withdraw(1).display_account_info()  #TODO why is int not callable?
