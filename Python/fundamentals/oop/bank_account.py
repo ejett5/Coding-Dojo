9f4edea00be4ea70364d1efd058a5e03217c8a5b
ejett5 <minijett5@gmail.com>
2024-01-10 12:00:24 -0500

    pushing updates -bank_account file still not functioning as expected

--- Python/fundamentals/oop/bank_account.py
+++ Python/fundamentals/oop/bank_account.py
@@ -1,3 +1,5 @@
+print("Hello World!")
+
 class BankAccount:
     # don't forget to add some default values for these parameters!
     def __init__(self, int_rate, balance, withdraw, deposit, yield_interest): 
@@ -6,13 +8,16 @@ class BankAccount:
         self.withdraw = withdraw
         self.deposit = deposit
         self.yield_interest = yield_interest
+        # self.amount = amount
         # self.user_name = user
+        # print()
 
-    def deposit(self, amount):
-
+    def deposit(self, deposit):
+        """string"""
         if(self.balance >= 0):
-            self.balance += amount #TODO link the amount to an var input
-        else:
+            self.balance += deposit #TODO link the amount to update balance
+            # print(self.balance)
+        else: 
             print("Please reach out to our representatives at: 555-555-5555")
 
 # amount to be withdrawn and update bank balance
@@ -24,15 +29,15 @@ class BankAccount:
         else:
             print("insufficient funds, your account will now be charged a $5 fee")
 
-        
-    def display_account_info(self):
-
-        print(self.balance, self.interest_rate, self.yield_interest, self.deposit) 
-
-        
+    
     def yield_interest(self):
 
         self.interest_rate = self.interest_rate * self.balance
+
+    def display_account_info(self):
+
+        print(self.interest_rate, self.balance,self.withdraw, self.deposit, self.yield_interest) 
+        # return "pilot"
         
 jm_toyota = BankAccount('18', '100', '20', '1', '3.5')
 print(jm_toyota.display_account_info())
