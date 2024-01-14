c426145987b6bb70dd4ab1e34d80cbfa982357d5
ejett5 <minijett5@gmail.com>
2024-01-14 15:43:11 -0500

    init playground assign files

---
+++ Python/Flask/playground/server.py
@@ -0,0 +1,11 @@
+# import statements, maybe some other routes
+    
+@app.route('/success')
+def success():
+  return "success"
+    
+    # app.run(debug=True) should be the very last statement! 
+
+#allow app to run blue box and set number of boxes to display
+
+#allow user to change color of boxes as well
