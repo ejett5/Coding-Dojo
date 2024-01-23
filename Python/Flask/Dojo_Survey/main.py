09710472d49390052e2d183bb59eb5be32add630
ejett5 <minijett5@gmail.com>
2024-01-23 02:04:30 -0500

    dojo survey best attempt - error 400 unsolved

--- Python/Flask/Dojo_Survey/main.py
+++ Python/Flask/Dojo_Survey/main.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session, request
 
-app = Flask(__name__,template_folder='Template') #need to have template_folder with template folder name for it to retrieve the index.html file
-@app.route('/')
+app = Flask(__name__,template_folder='Template', static_folder='static') #need to have template_folder with template folder name for it to retrieve the index.html file
+@app.route('/', methods =['GET','POST'])#
 def home():
+    session['Name'] = request.form['Name']
+    session['Programming Language'] = request.form['Programming Language']
+    session['Dojo Location'] = request.form['Dojo Location']
     return render_template('index.html')
+    return redirect('/result/')
     # print(render_template('index.html')) #working on console debugger method to find fail points similar to console.log in js
 
-if __name__ =="__main__":
+# route for rendering submitted info
+@app.route('/result/')
+def process():
+    return render_template('result.html')
+
+if __name__ =='__main__':
     app.run(debug=True, port= 5000)
