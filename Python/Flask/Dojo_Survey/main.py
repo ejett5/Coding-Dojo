30b79d2a998661ea351ef009ebd674055cb7dbff
ejett5 <minijett5@gmail.com>
2024-01-24 14:16:45 -0500

    added class Suvery_Results

--- Python/Flask/Dojo_Survey/main.py
+++ Python/Flask/Dojo_Survey/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, session, request
 
+
 app = Flask(__name__,template_folder='Template', static_folder='static') #need to have template_folder with template folder name for it to retrieve the index.html file
 @app.route('/', methods =['GET','POST'])#
 def home():
@@ -10,10 +11,23 @@ def home():
     return redirect('/result/')
     # print(render_template('index.html')) #working on console debugger method to find fail points similar to console.log in js
 
+#creating class to hold the survey results
+class Survey_Results:
+    DB = "Local instance MySQL83"
+    def __init__(self, data):
+        self.id = data[id]
+        self.Name = data['Name']
+        self.Location = data['Location']
+        self.Language = data['Favorite Language']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        
+
 # route for rendering submitted info
 @app.route('/result/')
 def process():
     return render_template('result.html')
+
 
 if __name__ =='__main__':
     app.run(debug=True, port= 5000)
