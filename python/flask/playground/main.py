574342a5cce54211ca12081a7e85c039d152fbc5
ejett5 <minijett5@gmail.com>
2024-02-08 16:04:30 -0500

    added routes to display phrases and number to display phrase and file just for the play route

--- python/flask/playground/main.py
+++ python/flask/playground/main.py
@@ -3,13 +3,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# main route for homepage
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return "Hallo, Wie gehts? Microsoft technologie hasse mich ."
 
 
+# # main route for homepage
+@app.route('/home/')
+# @app.route('/play/<color>/<number>/')
+def home():
+    return render_template('index.html')
+
+# make a page view that displays color boxes and repeats them
+@app.route('/play/<phrase>/<int:number>/')
+# @app.route('/play/<x>/<y>')
+def box_playground(phrase, number):
+    return render_template('game.html', phrase = phrase, number = number)
 
 # final line of code that runs the preceeding code
 if __name__ == "__main__":
-    app.run(debug=True, localhost = 8443)
+    app.run(debug=True, port= 8443)
