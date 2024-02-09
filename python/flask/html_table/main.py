ec7454f4e68996ac1c9c88e0c7fb6c98a21ef85e
ejett5 <minijett5@gmail.com>
2024-02-09 14:27:04 -0500

    may work not sure, having technical issues to run file

--- python/flask/html_table/main.py
+++ python/flask/html_table/main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from players import users
 
 app = Flask( __name__ )
 
 @app.route('/')
+def home():
+    return render_template('index.html')
 
 
 
