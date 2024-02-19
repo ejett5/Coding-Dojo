a0bebde351e72d1bca1046849284221089faf94b
ejett5 <minijett5@gmail.com>
2024-02-19 17:39:52 -0500

    web pages to submit and view and class is created - have ever in main.py cannot import flask_app

---
+++ python/flask/email_validation/flask_app/models/email_class.py
@@ -0,0 +1,21 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app import DB
+from flask import flash
+import re
+
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+class Email():
+    DB = 'email_validation_schema'
+    def __init__(self, data):
+        self.id = data['id']
+        self.email = data['email']
+
+        @classmethod
+        def submit_email(cls, data):
+            query = """ INSERT INTO email (email)
+            VALUES( %(email)s );
+            """
+            result = connectToMySQL('email_validation_schema').query_db(query, data)
+            return result
