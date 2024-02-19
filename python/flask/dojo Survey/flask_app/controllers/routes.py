432f71652efe50e491d72813813a81bd0c2a699d
ejett5 <minijett5@gmail.com>
2024-02-19 09:40:40 -0500

    moved init to within flask_app folder

--- python/flask/dojo Survey/flask_app/controllers/routes.py
+++ python/flask/dojo Survey/flask_app/controllers/routes.py
@@ -1,5 +1,5 @@
 from flask_app import app 
-from flask import render_template,redirect, request, session, flash
+from flask import render_template, redirect, request, session, flash
 from flask_app.models.dojos import Dojo 
 from flask_app.models.ninjas import Ninjas 
 
